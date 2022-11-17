ad867522b73ba4149f67c44026961deb8f5ec848
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-11-17 19:12:09 +0300

    Added second task

--- Homework/Task2/Program.cs
+++ Homework/Task2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: 
+
+Задайте значения M и N. 
+Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string message) {
+    bool isCorrect = false;
+    int result = 0;
+    Console.WriteLine(message);
+    while (!isCorrect || result < 1) {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect || result < 1)
+            Console.WriteLine("Ввели не натуральное число или оно слишком большое. Введите снова: ");
+    }
+    return result;
+}
+
+int SumInteger(int start, int fin) {
+    if (start == fin) {
+        return fin;
+    } else if (start > fin) {
+        start += SumInteger(start - 1, fin);
+    } else
+        start += SumInteger(start+1, fin);
+    return start;
+}
+
+Console.WriteLine("Cумма чисел в промежутке между ними: {0}",
+    SumInteger(
+        GetNumber("Введите первое число"),
+        GetNumber("Введите второе число")
+    )
+);
