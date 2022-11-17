6873d2479687f1a5a013489adbf36230da81c313
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-11-17 14:01:44 +0300

    Added first task

--- Homework/Task1/Program.cs
+++ Homework/Task1/Program.cs
@@ -7,3 +7,25 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+
+int GetNumber(string message) {
+    bool isCorrect = false;
+    int result = 0;
+    Console.WriteLine(message);
+    while (!isCorrect) {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect) 
+            Console.WriteLine("Ввели не число или оно слишком большое. Введите заново: ");
+    }
+    return result;
+}
+
+void Integers(int num) {
+    Console.Write("{0} ", num);
+    if (num != 1)
+        Integers(num - 1);
+}
+
+Integers(
+    GetNumber("Сколько натуральных чисел показать?")
+);
